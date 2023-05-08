20190cfa23445742b5ce333652abf2598d7cb748
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-05-07 23:24:56 -0300

    fix: Failed to load resource: net::ERR_INTERNET_DISCONNECTED

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,10 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-// const URL = import.meta.env.VITE_API_URL; // example to use .env 
-const URL = 'https://movies.slideworks.cc/movies'; 
+// const URL = import.meta.env.VITE_API_URL; // example to use .env
+const URL = "https://movies.slideworks.cc/movies";
 
 const api = axios.create({
   baseURL: URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*", // example to use cors
+    "Access-Control-Allow-Headers": "*", // example to use cors
+  },
 });
 
 export default api;
